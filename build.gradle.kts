40880157fbe36421f1f7c1b4484249b9a5669839
MinQDU <2024513@daelim.ac.kr>
2024-10-24 20:14:47 +0900

    deploy to swarm 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,9 @@ version = "0.0.1-SNAPSHOT"
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
 }
-
 repositories {
 	mavenCentral()
 }
-
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
